4c0019d21fcea106d31ea52e34697521faa83d35
xiongjie <xiongjie.cn@gmail.com>
2020-03-03 10:03:54 +0800

    代码优化

--- trunk/src/main/java/com/gserver/core/AnnotationScanner.java
+++ trunk/src/main/java/com/gserver/core/AnnotationScanner.java
@@ -47,12 +47,13 @@ public class AnnotationScanner implements BeanPostProcessor, PriorityOrdered {
                             beforeInterceptors.add(interceptor);
                         }
                     }
-                    Interceptor interceptor = null;
+
                     if (beforeInterceptors.size() > 1) {
                         for (int i = beforeInterceptors.size() - 1; i > 0; i--) {
                             beforeInterceptors.get(i - 1).setNext(beforeInterceptors.get(i));
                         }
                     }
+                    Interceptor interceptor = null;
                     if (beforeInterceptors.size() > 0) {
                         interceptor = beforeInterceptors.get(0);
                     }
